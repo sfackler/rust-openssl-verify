72e99bc137d59a29f8d6d4e650125600e6301cff
Steven Fackler <sfackler@gmail.com>
2016-08-11 23:22:37 -0700

    Release v0.2.0

---
+++ doc/v0.2.0/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["openssl_verify"] = {"doc":"Hostname verification for OpenSSL.","items":[[5,"verify_callback","openssl_verify","A convenience wrapper around verify_hostname that implements the logic for\nOpenSSL&#39;s certificate verification callback.",null,{"inputs":[{"name":"str"},{"name":"bool"},{"name":"x509storecontext"}],"output":{"name":"bool"}}],[5,"verify_hostname","","Validates that the certificate matches the provided fully qualified domain\nname.",null,{"inputs":[{"name":"str"},{"name":"x509ref"}],"output":{"name":"bool"}}]],"paths":[]};
+initSearch(searchIndex);
